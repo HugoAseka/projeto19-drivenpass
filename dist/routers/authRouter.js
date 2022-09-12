1d931f64dbd8871dd234ab5930daaee457ec2cd6
Hugo M Aseka <82669254+HugoAseka@users.noreply.github.com>
2022-09-12 10:19:37 -0300

    Delete dist directory

--- dist/routers/authRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as authController from "../controllers/authController.js";
-import validateObj from "../middlewares/validate.js";
-import authSchema from "../schemas/authSchema.js";
-var authRoute = Router();
-authRoute.post("/cadastrar", validateObj(authSchema), authController.signUp);
-authRoute.post("/login", validateObj(authSchema), authController.signIn);
-export default authRoute;
--- dist/routers/indexRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authRoute from "./authRouter.js";
-import cardRouter from "./cardRouter.js";
-import credentialRouter from "./credentialRouter.js";
-import notesRouter from "./notesRouter.js";
-import wifiRouter from "./wifiRouter.js";
-var router = Router();
-router.use(authRoute, credentialRouter, notesRouter, cardRouter, wifiRouter);
-export default router;
